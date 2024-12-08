36a22205562f2b8e7a07d3334902c6966518fc88
Gukin Han <gukin.dev@gmail.com>
2024-12-08 16:38:22 +0900

    Spring Cloud Gateway2 end

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,15 +1,24 @@
 package com.example.userservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class UserController {
 
     @GetMapping("/hello")
     public String getHello() {
         return "hello";
+    }
+
+    @GetMapping("/message")
+    public String getMessage(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in First Service";
     }
 }
